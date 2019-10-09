546d7fb85fe848876e18a659c09c4c36b3511307
katerinoc <47535616+katerinoc@users.noreply.github.com>
2019-10-09 16:41:03 +0300

    'added'

--- lab_1/unit.py
+++ lab_1/unit.py
@@ -4,6 +4,7 @@ import htmlCrawler
 import requests
 
 class Test_html(unittest.TestCase):
+
     def test_structure_url_2(self):
         f = open("articles.json", encoding='UTF-8')
         json_str = f.read()
@@ -14,8 +15,16 @@ class Test_html(unittest.TestCase):
         self.assertEqual(dict_keys,keys)
         self.assertIsNone(json_dict.get('title'))
 
-    #def get_titles(self):
-     #   htmlCrawler.find_articles('Google Новости - Бизнес - Последние.html')#возвращается список articles
+    def get_titles(self):
+        articles = htmlCrawler.find_articles('SavedHtml.html')#возвращается список articles
+        json_file = htmlCrawler.publish_report('SavedHtml.html')
+
+        json_str = json_file.read()
+        json_file.close()
+        json_dict = json.loads(json_str)
+        for i in range(len(json_dict["articles"])):
+            self.assertEqual(json_dict["articles"][i]["title"], articles[i])
+    
 
 
     def get_html_page(self):
@@ -25,6 +34,7 @@ class Test_html(unittest.TestCase):
         json_dict = json.loads(json_str)
         url_request = requests.get(json_dict['url'])
         self.assertEqual(url_request.status_code, 200)
+
 
 if __name__== '__main__':
     unittest.main()

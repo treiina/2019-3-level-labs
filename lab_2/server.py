b4fde5916b5589c50bb2d1f93a96704f092d339f
katerinoc <47535616+katerinoc@users.noreply.github.com>
2019-10-15 21:24:57 +0300

    'added'

--- lab_2/server.py
+++ lab_2/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
+#@app.route("/index")
 def open_html_page(url="https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FuSjFHZ0pTVlNnQVAB?hl=ru&gl=RU&ceid=RU%3Aru"):
     htmlCrawler.publish_report(url)
     f = open("C:/Users/Катя/PycharmProjects/2019-3-level-labs/articles.json", encoding='UTF-8')
@@ -17,13 +17,9 @@ def open_html_page(url="https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyM
     json_dict = json.loads(json_str)
     return render_template("index.html", data=json_dict)
 
-@app.route('/index')
-def my_link(url="https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FuSjFHZ0pTVlNnQVAB?hl=ru&gl=RU&ceid=RU%3Aru"):
-    htmlCrawler.publish_report(url)
-    f = open("C:/Users/Катя/PycharmProjects/2019-3-level-labs/articles.json", encoding='UTF-8')
-    json_str = f.read()
-    f.close()
-    json_dict = json.loads(json_str)
-    return redirect(url_for("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FuSjFHZ0pTVlNnQVAB?hl=ru&gl=RU&ceid=RU%3Aru"),data=json_dict)
+@app.route('/refresh', methods=['POST'])
+def refresh():
+    return redirect(url_for('open_html_page'))
+
 if __name__ == "__main__":
     app.run()
